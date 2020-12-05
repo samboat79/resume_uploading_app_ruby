cd5a337582a30af2ff0182d6d6ae3f7be5eaef63
sbsamboat9 <sbsamboat9@icloud.com>
2020-12-05 05:25:17 -0500

    commiting config rout changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   resources :resumes
-  root "resumes#index"
+  root to: "resumes#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
